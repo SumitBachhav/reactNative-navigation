64d06fee50edd201cd320e5c80155c03ab3e4321
SumitBachhav <sumitb2156@gmail.com>
2024-02-13 07:56:02 +0530

    complete

---
+++ src/components/Seperator.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+const Seperator = () => 
+ {
+    return (
+      <View style = {styles.seperator}>
+      </View>
+    )
+  }
+
+
+const styles = StyleSheet.create({
+    seperator : {
+        height: 0.8,
+        backgroundColor: '#CAD5E2'
+    }
+})
+
+export default Seperator
---
+++ src/screens/Home.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Text, StyleSheet, View, FlatList, Pressable } from 'react-native'
+
+//react navigation
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackPramList } from '../App'
+
+import ProductItem from '../components/ProductItem'
+import Seperator from '../components/Seperator'
+
+//data
+import { PRODUCTS_LIST } from '../data/constants'
+
+type HomeProps = NativeStackScreenProps<RootStackPramList, "Home">
+
+const Home = ({navigation}: HomeProps)  =>{
+  
+    return (
+      <View style = {styles.container}>
+        <FlatList
+        data = {PRODUCTS_LIST}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={Seperator}
+        renderItem={({item}) => (
+            <Pressable
+            onPress={() => {
+                navigation.navigate('Details', {
+                    product: item
+                })
+            }}
+            >
+                <ProductItem product={item}/>
+            </Pressable>
+        )}
+        />
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+
+        padding: 12,
+        backgroundColor: '#FFFFFF'
+    }
+})
+
+export default Home
